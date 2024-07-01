c5607df6ceccfe5d2350a041b9daca13403466e3
IsegunFagbemi <isegunoluwafagbemi1@gmail.com>
2024-07-01 16:25:02 -0700

    coin

--- Simple City/Assets/Scripts/Coin Generator.cs
+++ Simple City/Assets/Scripts/Coin Generator.cs
@@ -6,26 +6,45 @@ using TMPro;
 
 public class CoinGenerator : MonoBehaviour
 {
-    public int coins;
+    public int Coins;
     public TMP_Text coinUI;
     public CoinManager[] CoinManager;
     public GameObject[] PanelsGO;
     public Template[] Panels;
+    protected float Timer;
+    public int DelayAmount = 1;
+    //public RoadManager roadManager[];
 
     // Start is called before the first frame update
     void Start()
     {
-       for (int i = 0; i < CoinManager.Length; i++)
-          PanelsGO[i].SetActive(true);
-       coinUI.text = "Coins: " + coins.ToString();
-        //LoadPanels();
+       coinUI.text = "Coins: " + Coins.ToString();
     }
 
     public void AddCoins()
     {
-        coins++;
-        coinUI.text = "Coins: " + coins.ToString();
+        Timer += Time.deltaTime;
+        if (Timer >= DelayAmount)
+		{
+			Timer = 0f;
+			Coins++; 
+            coinUI.text = "Coins: " + Coins.ToString();
+        }
+        
+        Coins++;
+        
     }
+
+    //public void CheckPurchaseable()
+    //{
+        //for (int i = 0; i < CoinManager.Length; i++)
+        //{
+            //if (coins >= CoinManager[i].baseCost)
+                //roadManager[i].interactable = true;
+            //else
+                //roadManager[i].interactable = false;
+       //}
+    //}
 
     // Update is called once per frame
     void Update()
